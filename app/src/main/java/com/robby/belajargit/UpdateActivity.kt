22957c3323513383c657e6f204316ff6acb485ac
Bagicode <bagicode@gmail.com>
2022-02-16 17:47:00 +0700

    update ui

---
+++ app/src/main/java/com/robby/belajargit/UpdateActivity.kt
@@ -0,0 +1,11 @@
+package com.robby.belajargit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }
+}
